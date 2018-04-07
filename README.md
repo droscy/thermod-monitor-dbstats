d07abe95e4e00408888414982f95dae5dafc03f1
Simone Rossetto <simros85@gmail.com>
2018-04-07 08:53:54 +0200

    Add draft of setup script

---
+++ README.md
@@ -0,0 +1,42 @@
+# Thermod DB-Stats monitor
+This monitor collects statistics on Thermod operation.
+
+## License
+Thermod DB-Stats monitor v0.0.0 \
+Copyright (C) 2018 Simone Rossetto <simros85@gmail.com> \
+GNU General Public License v3
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+## How to install
+
+### Requirements
+*Thermod DB-Stats monitor* requires [Python3](https://www.python.org/)
+(at least version 3.5) and the packages:
+
+ - [thermod](https://github.com/droscy/thermod) (>=1.0.0)
+ - [requests](http://docs.python-requests.org/) (>=2.4.3)
+
+
+### Installation
+To install *Thermod DB-Stats monitor* first uncompress the tarball and run
+
+```bash
+python3 setup.py install
+```
+
+
+## Starting/Stopping the monitor
+TODO
---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""Setup script for Thermod DB-Stats monitor.
+
+@author:     Simone Rossetto
+@copyright:  2018 Simone Rossetto
+@license:    GNU General Public License v3
+@contact:    simros85@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from setuptools import setup
+
+def get_version():
+    main_ns = {}
+    with open('thermod-monitor-dbstats','r') as version_file:
+        exec(version_file.read(), main_ns)  # TODO non funziona perché è uno script!!
+    return main_ns['__version__']
+
+setup(name='thermod-monitor-dbstats',
+      version=get_version(),
+      description='Thermod DB-Stats monitor.',
+      long_description='This monitor collects statistics on Thermod operation',
+      author='Simone Rossetto',
+      author_email='simros85@gmail.com',
+      url='https://github.com/droscy/thermod-monitor-dbstats',
+      license = 'GPL-3.0+',
+      scripts=['thermod-monitor-dbstats'],
+      install_requires=['thermod >= 1.0.0',
+                        'requests >= 2.4.3'])
+
+# vim: fileencoding=utf-8 tabstop=4 shiftwidth=4 expandtab
